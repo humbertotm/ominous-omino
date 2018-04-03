fa395886a5008ce8a04bbbc0c66b8857c23f79bc
humbertotm <humberto.htm@gmail.com>
2018-04-03 15:28:11 -0700

    Added last condition.

--- ominous_omino.rb
+++ ominous_omino.rb
@@ -2,19 +2,43 @@ $RICHARD = "RICHARD"
 $GABRIEL = "GABRIEL"
 
 def even_n_r(x, r, c)
-  return x < 7 && r >= x && (r * c) % x == 0 && x % 2 == 0 && c >= x / 2
+  if (x < 7 && r >= x && (r * c) % x == 0 && x % 2 == 0 && c >= x / 2)
+    if (x > 3)
+      return (((r * c) - x * (x / 2)) != 0 && ((r * c) - x * (x / 2)) % x == 0 && c > (x / 2))
+    else
+      return true
+    end
+  end
 end
 
 def even_n_c(x, r, c)
-  return x < 7 && c >= x && (r * c) % x == 0 && x % 2 == 0 && r >= x / 2
+  if (x < 7 && c >= x && (r * c) % x == 0 && x % 2 == 0 && r >= x / 2)
+    if (x > 3)
+      return (((r * c) - x * (x / 2)) != 0 && ((r * c) - x * (x / 2)) % x == 0 && r > (x / 2))
+    else
+      return true
+    end
+  end
 end
 
 def odd_n_r(x, r, c)
-  return x < 7 && r >= x && (r * c) % x == 0 && x % 2 != 0 && c >= (x / 2) + 1
+  if (x < 7 && r >= x && (r * c) % x == 0 && x % 2 != 0 && c >= (x / 2) + 1)
+    if (x > 3)
+      return (((r * c) - x * ((x / 2) + 1)) != 0 && ((r * c) - x * ((x / 2) + 1)) % x == 0 && c > (x / 2))
+    else
+      return true
+    end
+  end
 end
 
 def odd_n_c(x, r, c)
-  return x < 7 && c >= x && (r * c) % x == 0 && x % 2 != 0 && r >= (x / 2) + 1
+  if (x < 7 && c >= x && (r * c) % x == 0 && x % 2 != 0 && r >= (x / 2) + 1)
+    if (x > 3)
+      return (((r * c) - x * ((x / 2) + 1)) != 0 && ((r * c) - x * ((x / 2) + 1)) % x == 0 && r > (x / 2))
+    else
+      return true
+    end
+  end
 end
 
 def predict_who_wins(x, r, c, test_case)
